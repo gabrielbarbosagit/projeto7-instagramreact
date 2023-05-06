58bd6398ed2712c82acac0508fccc231f28fb3d6
gabriel barbosa <gabrielb_lima@hotmail.com>
2023-05-06 20:54:11 -0300

    teste double click no post - like

--- src/components/Post.js
+++ src/components/Post.js
@@ -8,23 +8,36 @@ export default function Post(props) {
   const [color, setColor] = useState('#000000');
   const [numberLike, setNumberLike] = useState(() => props.post.numberPeople);
   const [liked, setLiked] = useState(false);
+  const [canUnlike, setCanUnlike] = useState(false); // novo estado
 
   function likePost() {
-    if (liked) {
-      
+    if (liked) { // Verifica se a imagem já foi curtida
+      return; // Se já foi curtida, não faz nada
+    }
+    if (heart === 'heart') {
       setHeart('heart-outline');
       setColor('#000000');
       setNumberLike(numberLike - 1);
       setLiked(false);
-    } else {
-      
+      setCanUnlike(true); // atualiza o estado canUnlike para true
+    } else if (heart === 'heart-outline') {
       setHeart('heart');
       setColor('#FF0000');
       setNumberLike(numberLike + 1);
       setLiked(true);
+      setCanUnlike(true); // atualiza o estado canUnlike para true
     }
   }
-  
+
+  function unlikePost() {
+    if (canUnlike) { // verifica se o usuário pode retirar o like
+      setHeart('heart-outline');
+      setColor('#000000');
+      setNumberLike(numberLike - 1);
+      setLiked(false);
+      setCanUnlike(false); // atualiza o estado canUnlike para false
+    }
+  }
 
   return (
     <div data-test="post" className="post">
@@ -46,29 +59,30 @@ export default function Post(props) {
       <div className="fundo">
         <div className="acoes">
           <div>
-            <ion-icon
-              data-test="like-post"
-              name={heart}
-              style={{ color: color }}
-              onClick={likePost}
+            <ion-icon data-test="like-post" name={heart} style={{ color: color }} onClick={() => {
+
+              if (liked) {
+                unlikePost();
+              } else {
+                likePost();
+              }
+            }}
             ></ion-icon>
             <ion-icon name="chatbubble-outline"></ion-icon>
             <ion-icon name="paper-plane-outline"></ion-icon>
           </div>
 
           <div>
-            <ion-icon
-              class="mark"
-              data-test="save-post"
-              onClick={() => {
-                setBookmark(!bookmark);
-              }}
+            <ion-icon class="mark" data-test="save-post" onClick={() => { setBookmark(!bookmark); }}
+
               name={bookmark === true ? 'bookmark-outline' : 'bookmark'}
+
             ></ion-icon>
           </div>
         </div>
 
         <div className="curtidas">
+
           <img src={imageFollower} alt={imageFollower} target="_blank" rel="noreferrer" />
 
           <div data-test="likes-number" className="texto">
